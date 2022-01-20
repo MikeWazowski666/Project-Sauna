ab26f5b00d9381c1b983e9332ba9b6aa654696cc
MOliver <59602119+MikeWazowski666@users.noreply.github.com>
2022-01-20 14:28:41 +0200

    Added Arduino example code

---
+++ arduino/example.ino
@@ -0,0 +1,39 @@
+// Include the required Arduino libraries:
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Define to which pin of the Arduino the 1-Wire bus is connected:
+#define ONE_WIRE_BUS 5
+
+// Create a new instance of the oneWire class to communicate with any OneWire device:
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass the oneWire reference to DallasTemperature library:
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  // Begin serial communication at a baud rate of 9600:
+  Serial.begin(9600);
+  // Start up the library:
+  sensors.begin();
+}
+
+void loop() {
+  // Set the temperature resolution to 12 bits
+  sensors.setResolution(12);
+  
+  // Send the command for all devices on the bus to perform a temperature conversion:
+  sensors.requestTemperatures();
+  
+  // Fetch the temperature in degrees Celsius for device index:
+  float tempC = sensors.getTempCByIndex(0);
+
+  // Print the temperature in Celsius in the Serial Monitor:
+  Serial.print("Temperature: ");
+  Serial.print(tempC);
+  Serial.print(" \xC2\xB0"); // shows degree symbol
+  Serial.println("C");
+
+  // Wait 1 second:
+  delay(1000);
+}
